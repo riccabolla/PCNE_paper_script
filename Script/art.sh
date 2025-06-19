cf89cd5b8e2727b6ad1f2065ba7d306949a0424e
riccabolla <bollach@virgilio.it>
2025-06-19 15:08:07 +0200

    Update art.sh

--- Script/art.sh
+++ Script/art.sh
@@ -2,6 +2,8 @@
 
 conda activate art
 
+mkdir -p Sim_30x Sim_50x
+
 cd Sim_30x/
 art_illumina -ss MSv3 -p -l 150 -m 500 -s 50 -f 30 -i ../CP003200.1_chrom.fasta -o chrom_30x_
 art_illumina -ss MSv3 -p -l 150 -m 500 -s 50 -f 30 -i ../CP003223.1_plasmid.fasta -o plasmid_1x_
@@ -9,8 +11,10 @@ art_illumina -ss MSv3 -p -l 150 -m 500 -s 50 -f 60 -i ../CP003223.1_plasmid.fast
 art_illumina -ss MSv3 -p -l 150 -m 500 -s 50 -f 150 -i ../CP003223.1_plasmid.fasta -o plasmid_5x_
 
 rm *.aln
+
 rename 's/_1\./_R1./' *.fq
 rename 's/_2\./_R2./' *.fq
+
 cat chrom_30x_R1.fq plasmid_1x_R1.fq > Sim_30x_1copy_R1.fastq
 cat chrom_30x_R2.fq plasmid_1x_R2.fq > Sim_30x_1copy_R2.fastq
 cat chrom_30x_R1.fq plasmid_2x_R1.fq > Sim_30x_2copy_R1.fastq
@@ -22,14 +26,17 @@ gzip *.fastq
 rm *.fq
 
 cd ../Sim_50x/
+
 art_illumina -ss MSv3 -p -l 150 -m 500 -s 50 -f 50 -i ../CP003200.1_chrom.fasta -o chrom_50x_
 art_illumina -ss MSv3 -p -l 150 -m 500 -s 50 -f 50 -i ../CP003223.1_plasmid.fasta -o plasmid_1x_
 art_illumina -ss MSv3 -p -l 150 -m 500 -s 50 -f 100 -i ../CP003223.1_plasmid.fasta -o plasmid_2x_
 art_illumina -ss MSv3 -p -l 150 -m 500 -s 50 -f 250 -i ../CP003223.1_plasmid.fasta -o plasmid_5x_
 
 rm *.aln
+
 rename 's/_1\./_R1./' *.fq
 rename 's/_2\./_R2./' *.fq
+
 cat chrom_50x_R1.fq plasmid_1x_R1.fq > Sim_50x_1copy_R1.fastq
 cat chrom_50x_R2.fq plasmid_1x_R2.fq > Sim_50x_1copy_R2.fastq
 cat chrom_50x_R1.fq plasmid_2x_R1.fq > Sim_50x_2copy_R1.fastq
