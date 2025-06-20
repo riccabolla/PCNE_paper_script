2cc1b7da73d015a0554b4b1f779a2e24a98b022d
riccabolla <bollach@virgilio.it>
2025-06-20 13:39:13 +0200

    Create shovill.sh

---
+++ Script/shovill.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+conda activate shovill
+
+READS_DIR="Sim_30x"
+OUTDIR="Shovill_30x"
+
+for f1 in $READS_DIR/*_R1.fastq.gz; do
+  f2="${f1/_R1.fastq.gz/_R2.fastq.gz}"
+  sample_name=$(basename "$f1" _R1.fastq.gz)
+  out="$OUTDIR/$sample_name"
+  shovill --R1 "$f1" --R2 "$f2" --outdir "$out" --cpus 10 --ram 16 --assembler spades
+  mv "$out/spades.fasta" "$out/${sample_name}.fasta"
+done
+
