212a082044d0cdd8b32b4d8a803e5ce5c7d978fb
riccabolla <bollach@virgilio.it>
2025-06-28 16:19:01 +0200

    Update shovill.sh

--- Script/shovill.sh
+++ Script/shovill.sh
@@ -9,7 +9,7 @@ for f1 in $READS_DIR/*_R1.fastq.gz; do
   f2="${f1/_R1.fastq.gz/_R2.fastq.gz}"
   sample_name=$(basename "$f1" _R1.fastq.gz)
   out="$OUTDIR/$sample_name"
-  shovill --R1 "$f1" --R2 "$f2" --outdir "$out" --cpus 10 --minlen 1000 --ram 16 --assembler spades
+  shovill --R1 "$f1" --R2 "$f2" --outdir "$out" --cpus 5 --minlen 1000 --ram 16 --assembler spades
   mv "$out/spades.fasta" "$out/${sample_name}.fasta"
 done
 
