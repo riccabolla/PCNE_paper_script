5e6b3e305d12652bfc70400f6fec2e3d35adf3af
riccabolla <bollach@virgilio.it>
2025-08-22 19:18:19 +0200

    Update pcne_mobsuite.sh PCNE v2.0.0

--- Script/pcne_mobsuite.sh
+++ Script/pcne_mobsuite.sh
@@ -2,18 +2,18 @@
 
 conda activate mobsuite
 
-INPUT_DIR="Shovill_30x" 
+INPUT_DIR="Shovill_50x" 
 OUTPUT_DIR="Mobsuite"
 
 for dir in "$INPUT_DIR"/*/; do
   dir_name=$(basename "$dir")
   mkdir -p "$OUTPUT_DIR/$dir_name"
-  mob_recon --infile "$dir/${dir_name}.fasta" --outdir "$OUTPUT_DIR/$dir_name/"
+  mob_recon --infile "$dir/${dir_name}.fasta" -n 5 --outdir "$OUTPUT_DIR/$dir_name/"
 done
 
 conda activate pcne
 
-READS="Sim_30x"
+READS="Sim_50x"
 
 for file_R1 in "$READS"/*_R1.fastq.gz; do
   file_R2="${file_R1/_R1.fastq.gz/_R2.fastq.gz}"
@@ -21,6 +21,6 @@ for file_R1 in "$READS"/*_R1.fastq.gz; do
   chrom="$OUTPUT_DIR/$sample_name/chromosome.fasta"
   plasmid="$OUTPUT_DIR/$sample_name/plasmid_AC125.fasta"
   pcne -c "$chrom" -p "$plasmid" -r "$file_R1" -R "$file_R2" -t 5 --single-plasmid --plot -o "$OUTPUT_DIR/$sample_name/${sample_name}_base"
-  pcne -c "$chrom" -p "$plasmid" -r "$file_R1" -R "$file_R2" -b -t 5 --single-plasmid --plot -o "$OUTPUT_DIR/$sample_name/${sample_name}_scg"
-  pcne -c "$chrom" -p "$plasmid" -r "$file_R1" -R "$file_R2" -b --gc-correction --gc-plot "$OUTPUT_DIR/$sample_name/${sample_name}_gc.png" -t 5 --single-plasmid --plot -o "$OUTPUT_DIR/$sample_name/${sample_name}_scg_filter_gc"
+  pcne -c "$chrom" -p "$plasmid" -r "$file_R1" -R "$file_R2" --gc-correction --gc-plot -t 5 --single-plasmid --plot -o "$OUTPUT_DIR/$sample_name/${sample_name}_gc_cor"
 done
+  
