9d15b1d5567d9d8ae129a3fa2b90a4a5397876a6
riccabolla <bollach@virgilio.it>
2025-08-22 19:15:30 +0200

    Update pcne_50x.sh PCNE v2.0.0

--- Script/pcne_50x.sh
+++ Script/pcne_50x.sh
@@ -12,7 +12,10 @@ mkdir -p $OUTPUT_50X
 for file_R1 in $INPUT_50X/*_R1.fastq.gz; do
   file_R2="${file_R1/_R1.fastq.gz/_R2.fastq.gz}"
   sample_name=$(basename "$file_R1" _R1.fastq.gz)
-  pcne -c $CHROMOSOME -p $PLASMID -r $INPUT_50X/${file_R1} -R $INPUT_50X/${file_R2} -t 5  --plot -o $OUTPUT_50X/${sample_name}_base 
-  pcne -c $CHROMOSOME -p $PLASMID -r $INPUT_50X/${file_R1} -R $INPUT_50X/${file_R2} -t 5 --plot -b -o $OUTPUT_50X/${sample_name}_scg 
-  pcne -c $CHROMOSOME -p $PLASMID -r $INPUT_50X/${file_R1} -R $INPUT_50X/${file_R2} -t 5 --plot -b --gc-correction --gc-plot $OUTPUT_50X/${sample_name}.png -Q 10 -F 2308 -o $OUTPUT_50X/${sample_name}_scg_filter_gc
-done  
+  pcne -c $CHROMOSOME -p $PLASMID -r ${file_R1} -R ${file_R2} -t 5  --plot -o $OUTPUT_50X/${sample_name}_base
+  pcne -c $CHROMOSOME -p $PLASMID -r ${file_R1} -R ${file_R2} -t 5 --gc-correction --gc-plot --plot -o $OUTPUT_50X/${sample_name}_gc_corrected
+done
+
+cd $OUTPUT_50X/
+pcne_summary
+  
