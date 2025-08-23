28886523c3f149fc9a8f222a96910faacf4a3222
riccabolla <bollach@virgilio.it>
2025-08-23 12:29:30 +0200

    Update shovill.sh PCNEv2.0.0

--- Script/shovill.sh
+++ Script/shovill.sh
@@ -2,14 +2,14 @@
 
 conda activate shovill
 
-READS_DIR="Sim_30x"
-OUTDIR="Shovill_30x"
+READS_DIR="Sim_50x"
+OUTDIR="Shovill_50x"
 
 for f1 in $READS_DIR/*_R1.fastq.gz; do
   f2="${f1/_R1.fastq.gz/_R2.fastq.gz}"
   sample_name=$(basename "$f1" _R1.fastq.gz)
   out="$OUTDIR/$sample_name"
-  shovill --R1 "$f1" --R2 "$f2" --outdir "$out" --cpus 5 --minlen 1000 --ram 16 --assembler spades
+  shovill --R1 "$f1" --R2 "$f2" --outdir "$out" --cpus 5 --ram 16 --assembler spades
   mv "$out/spades.fasta" "$out/${sample_name}.fasta"
 done
 
